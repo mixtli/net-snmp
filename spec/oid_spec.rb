d3c089a53404aecff45fda3a542f6d7fec338344
Jared Breeden <jared.breeden@gmail.com>
2014-10-31 16:28:55 -0400

    Removing some debug code

--- spec/oid_spec.rb
+++ spec/oid_spec.rb
@@ -1,12 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-module Net::SNMP::Inline
-  def self.oid_size
-    puts "FAKE OID SIZE RETRIEVED"
-    8
-  end
-end
-
 describe Net::SNMP::OID do
   it "should instantiate valid oid with numeric" do
     oid = Net::SNMP::OID.new("1.3.6.1.2.1.2.1.0")
