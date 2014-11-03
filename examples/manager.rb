46a94a852b9564f72b26bee67772ae1d3330852c
jbreeden <jared.breeden@gmail.com>
2014-11-02 20:08:48 -0500

    Updating manager example

--- examples/manager.rb
+++ examples/manager.rb
@@ -1,13 +1,10 @@
-$snmp_verbose = true
-require 'net-snmp-min'
+$: << '../lib'
 
-session = Net::SNMP::Session.open(:peername => "localhost", :community => "public" )
-begin
+require 'net-snmp'
+
+Net::SNMP::Session.open(:peername => "localhost", :community => "public" ) do |session|
   t_start = Time.now
   pdu = session.get(["1.3.6.1.4.1.290.6.7.3.1.3.6.0", '1.3.6.1.4.1.290.6.7.3.1.3.7.0'])
   t_end = Time.now
   puts "Got #{pdu.varbinds[0].value} & #{pdu.varbinds[1].value} in #{(t_end - t_start) * 1000} ms"
-rescue Exception => e
-  puts e.message
 end
-session.close
